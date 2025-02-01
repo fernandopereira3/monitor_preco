0e75ea401d3d9bd55221cae1ca2a87a6eb182b3f
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-02-01 20:01:54 -0300

    Lascou se

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 cd /home/fernando/repositorios/monitor_preco/
+python -m venv .
 source /home/fernando/repositorios/monitor_preco/bin/activate
 python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py
