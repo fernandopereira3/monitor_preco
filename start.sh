bb6b4a7909de9db89fa3c72b4ee3276e59d435c9
Fernando Pereira <fernandopereira-3@hotmail.com>
2025-02-01 20:05:21 -0300

    Ta f***

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 cd /home/fernando/repositorios/monitor_preco/
 python -m venv .
 source /home/fernando/repositorios/monitor_preco/bin/activate
+pip install -r requirements.txt
 python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py
