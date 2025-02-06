b4737abd170ebdc1067549d7db03357282c8f319
Fernando <fernandopereira-3@hotmail.com>
2025-02-06 08:10:27 -0300

    Tentar no linux

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
 date=$(date '+%Y-%m-%d')
 
-#cd /home/fernando/repositorios/monitor_preco/ #linux
-cd /Users/fernando/Repositorios_Git/monitor_preco/ #mac
+cd /home/fernando/repositorios/monitor_preco/ #linux
 python3 -m venv .
-#source /home/fernando/repositorios/monitor_preco/bin/activate #linux
-source /Users/fernando/Repositorios_Git/monitor_preco/bin/activate # mac
+source /home/fernando/repositorios/monitor_preco/bin/activate #linux
 pip install -r requirements.txt
 git add .
 git commit -m "$date"
 git push origin main
-#python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py #Linux
-python3 /Users/fernando/Repositorios_Git/monitor_preco/executar_monitoramento.py
+python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py #Linux
