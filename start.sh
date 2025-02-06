828b29c22f470a6d2b7aedd7385847e5fb744206
Fernando <fernandopereira-3@hotmail.com>
2025-02-06 07:51:59 -0300

    2025-02-06

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
-cd /home/fernando/repositorios/monitor_preco/
-python -m venv .
-source /home/fernando/repositorios/monitor_preco/bin/activate
+date=$(date '+%Y-%m-%d')
+
+#cd /home/fernando/repositorios/monitor_preco/ #linux
+cd /Users/fernando/Repositorios_Git/monitor_preco/ #mac
+python3 -m venv .
+#source /home/fernando/repositorios/monitor_preco/bin/activate #linux
+source /Users/fernando/Repositorios_Git/monitor_preco/bin/activate # mac
 pip install -r requirements.txt
-python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py
+git add .
+git commit -m "$date"
+git push orgin main
+#python3 /home/fernando/repositorios/monitor_preco/executar_monitoramento.py #Linux
+python3 /Users/fernando/Repositorios_Git/monitor_preco/executar_monitoramento.py
